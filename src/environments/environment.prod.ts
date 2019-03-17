db9ab91555db1f5723f8240c85e8284ca78e8caa
faisaljebali <faisaljebali@gmail.com>
2019-03-17 21:42:21 +0100

    first env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyAuAytKYhCYlZQYJNZWA-OsNlNBfUjqYb4',
-    authDomain: 'chat-f1e1d.firebaseapp.com',
-    databaseURL: 'https://chat-f1e1d.firebaseio.com',
-    projectId: 'chat-f1e1d',
-    storageBucket: 'chat-f1e1d.appspot.com',
-    messagingSenderId: '623720698023'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   }
 };
